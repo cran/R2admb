57eca3cebaec2a712a827b04ea1963f7a407934a
Ben Bolker <bolker@mcmaster.ca>
2020-03-09 15:30:02 +0000

    version 0.7.16.2

--- inst/doc/R2admb.R
+++ inst/doc/R2admb.R
@@ -1,25 +1,25 @@
-## ----opts,echo=FALSE,message=FALSE---------------------------------------
+## ----opts,echo=FALSE,message=FALSE--------------------------------------------
 library("knitr")
 opts_chunk$set(tidy=FALSE)
 
-## ----do_admb_fake,eval=FALSE---------------------------------------------
+## ----do_admb_fake,eval=FALSE--------------------------------------------------
 #  fitted.model <- do_admb(fn,data,params,
 #                          run.opts=run.control(checkparam="write",
 #                          checkdata="write"))
 
-## ----libs,message=FALSE--------------------------------------------------
+## ----libs,message=FALSE-------------------------------------------------------
 library("R2admb")
 library("ggplot2") ## for pictures
 theme_set(theme_bw())  ## cosmetic
 zmargin <- theme(panel.spacing=grid::unit(0,"lines"))
 library("bbmle")
 
-## ----dat1----------------------------------------------------------------
+## ----dat1---------------------------------------------------------------------
 ReedfrogSizepred <- 
   data.frame(TBL = rep(c(9,12,21,25,37),each=3),
              Kill = c(0,2,1,3,4,5,0,0,0,0,1,0,0,0,0L))
 
-## ----mlefit--------------------------------------------------------------
+## ----mlefit-------------------------------------------------------------------
 m0 <- mle2(Kill~dbinom(c*((TBL/d)*exp(1-TBL/d))^g,size=10),
            start=list(c=0.45,d=13,g=1),data=ReedfrogSizepred,
            method="L-BFGS-B",
@@ -27,26 +27,26 @@ m0 <- mle2(Kill~dbinom(c*((TBL/d)*exp(1-TBL/d))^g,size=10),
            upper=c(c=0.8,d=20,g=60),
            control=list(parscale=c(c=0.5,d=10,g=1)))
 
-## ----predvals------------------------------------------------------------
+## ----predvals-----------------------------------------------------------------
 TBLvec = seq(9.5,36,length=100)
 predfr <- 
   data.frame(TBL=TBLvec,
              Kill=predict(m0,newdata=data.frame(TBL=TBLvec)))
 
-## ----fig1,echo=FALSE-----------------------------------------------------
+## ----fig1,echo=FALSE----------------------------------------------------------
 g1  <- ggplot(ReedfrogSizepred,
               aes(x=TBL,y=Kill/10))+
     geom_point()+stat_sum(aes(size=..n..))+
-    geom_smooth(method="loess")+
+    geom_smooth(method="loess",formula=y~x)+
     labs(size="n",x="Size (total body length",
          y="Proportion killed")+
     coord_cartesian(ylim=c(-0.05,0.55))
 startest <- stat_function(fun = function(x) { 0.45*((x/13)*exp(1-x/13)) },
                           lty=2,colour="red")
-g1+startest+
-    geom_line(data=predfr,colour="purple",lty=2)
+print(g1+startest+
+      geom_line(data=predfr,colour="purple",lty=2))
 
-## ----admbfit_getruns,echo=FALSE------------------------------------------
+## ----admbfit_getruns,echo=FALSE-----------------------------------------------
 load_doc <- function(x) {
     ##    Define a bit of magic to load results from previous runs: 
     ## would like parent.frame(2) above instead of
@@ -56,17 +56,17 @@ load_doc <- function(x) {
 }
 zz <- load_doc("Reedfrog_runs.RData")
 
-## ----setup_admb,eval=FALSE-----------------------------------------------
+## ----setup_admb,eval=FALSE----------------------------------------------------
 #  setup_admb()
 
-## ----rfs_setup-----------------------------------------------------------
+## ----rfs_setup----------------------------------------------------------------
 rfs_params <- list(c=0.45,d=13,g=1) ## starting parameters
 rfs_bounds <- list(c=c(0,1),d=c(0,50),g=c(-1,25)) ## bounds
 rfs_dat <- c(list(nobs=nrow(ReedfrogSizepred),
                   nexposed=rep(10,nrow(ReedfrogSizepred))),
              ReedfrogSizepred)
 
-## ----admbfit_fake,eval=FALSE---------------------------------------------
+## ----admbfit_fake,eval=FALSE--------------------------------------------------
 #  m1 <- do_admb("ReedfrogSizepred0",
 #                data=rfs_dat,
 #                params=rfs_params,
@@ -77,7 +77,7 @@ rfs_dat <- c(list(nobs=nrow(ReedfrogSizepred),
 #           "reedfrogsizepred0_gen.tpl",
 #           "reedfrogsizepred0")) ## clean up leftovers
 
-## ----rffit2,eval=FALSE---------------------------------------------------
+## ----rffit2,eval=FALSE--------------------------------------------------------
 #  rfs_params$g <- 2
 #  m2 <- do_admb("ReedfrogSizepred1",
 #                data=rfs_dat,
@@ -86,22 +86,22 @@ rfs_dat <- c(list(nobs=nrow(ReedfrogSizepred),
 #                run.opts=run.control(checkparam="write",
 #                checkdata="write"))
 
-## ----basic---------------------------------------------------------------
+## ----basic--------------------------------------------------------------------
 m1
 
-## ----coef----------------------------------------------------------------
+## ----coef---------------------------------------------------------------------
 coef(m1)
 
-## ----summary-------------------------------------------------------------
+## ----summary------------------------------------------------------------------
 summary(m1)
 
-## ----vcov----------------------------------------------------------------
+## ----vcov---------------------------------------------------------------------
 vcov(m1)
 
-## ----others--------------------------------------------------------------
+## ----others-------------------------------------------------------------------
 c(logLik(m1),deviance(m1),AIC(m1))
 
-## ----profrun,eval=FALSE,tidy=FALSE---------------------------------------
+## ----profrun,eval=FALSE,tidy=FALSE--------------------------------------------
 #  m1P <- do_admb("ReedfrogSizepred0",
 #                 data=c(list(nobs=nrow(ReedfrogSizepred),
 #                 nexposed=rep(10,nrow(ReedfrogSizepred))),
@@ -114,10 +114,10 @@ c(logLik(m1),deviance(m1),AIC(m1))
 #                 workdir=".",
 #                 profile.opts=list(pars=c("c","d","g")))
 
-## ----mleprof,cache=TRUE--------------------------------------------------
+## ----mleprof,cache=TRUE-------------------------------------------------------
 m0prof <- profile(m0)
 
-## ----profcalcs2,echo=FALSE,warning=FALSE---------------------------------
+## ----profcalcs2,echo=FALSE,warning=FALSE--------------------------------------
 tmpf <- function(p,w="prof") {
   pp <- log(m1P$prof[[p]][[w]][,2])
   pp <- max(pp)-pp
@@ -141,14 +141,14 @@ pdat <- rbind(cbind(as.data.frame(m0prof),method="mle2"),
               cbind(profnlist,method="ADMB_norm"),
               cbind(quadlist,method="Wald"))
 
-## ----profpic,echo=FALSE,fig.width=8,fig.height=3.2,warning=FALSE---------
+## ----profpic,echo=FALSE,fig.width=8,fig.height=3.2,warning=FALSE--------------
 ggplot(pdat,aes(x=focal,y=abs(z),group=method,colour=method))+geom_line()+
       geom_point(alpha=0.5)+
   facet_grid(.~param,scale="free_x")+ylim(0,3)+xlab("")+
       ylab(expression(Delta(sqrt(-2*L))))+
       geom_hline(yintercept=1.96,lty=2)+zmargin
 
-## ----admbfakemc,eval=FALSE,tidy=FALSE------------------------------------
+## ----admbfakemc,eval=FALSE,tidy=FALSE-----------------------------------------
 #  m1MC <- do_admb("ReedfrogSizepred0",
 #                  data=rfs_dat,
 #                  params=rfs_params,
@@ -162,35 +162,35 @@ ggplot(pdat,aes(x=focal,y=abs(z),group=method,colour=method))+geom_line()+
 #           "reedfrogsizepred0_gen.tpl",
 #           "reedfrogsizepred0"))
 
-## ----mchistplot,message=FALSE--------------------------------------------
+## ----mchistplot,message=FALSE-------------------------------------------------
 plot(m1MC$hist)
 
-## ----coda----------------------------------------------------------------
+## ----coda---------------------------------------------------------------------
 library("coda")
 mmc <- as.mcmc(m1MC$mcmc)
 
-## ----mctraceplot---------------------------------------------------------
+## ----mctraceplot--------------------------------------------------------------
 library("lattice")
 xyplot(mmc)
 
-## ----rgdiags-------------------------------------------------------------
+## ----rgdiags------------------------------------------------------------------
 raftery.diag(mmc)
 geweke.diag(mmc)
 
-## ----gdiag,echo=FALSE----------------------------------------------------
+## ----gdiag,echo=FALSE---------------------------------------------------------
 gd <- geweke.diag(mmc)
 gd1 <- gd[["z"]][1]
 
-## ----effsize-------------------------------------------------------------
+## ----effsize------------------------------------------------------------------
 effectiveSize(mmc)
 
-## ----hpd-----------------------------------------------------------------
+## ----hpd----------------------------------------------------------------------
 HPDinterval(mmc)
 
-## ----mcdensplot----------------------------------------------------------
+## ----mcdensplot---------------------------------------------------------------
 densityplot(mmc)
 
-## ----lme4,message=FALSE--------------------------------------------------
+## ----lme4,message=FALSE-------------------------------------------------------
 library(lme4)
 if (as.numeric(R.version$major)<3) {
 ## FIXME
@@ -198,19 +198,19 @@ if (as.numeric(R.version$major)<3) {
                  family = binomial, data = cbpp)
 }
 
-## ----toymats-------------------------------------------------------------
+## ----toymats------------------------------------------------------------------
 X <- model.matrix(~period,data=cbpp)
 Zherd <- model.matrix(~herd-1,data=cbpp)
 
-## ----toydat--------------------------------------------------------------
+## ----toydat-------------------------------------------------------------------
 tmpdat <- list(X=X,Zherd=Zherd,
                  incidence=cbpp$incidence,size=cbpp$size,
                  nobs=nrow(cbpp))
 
-## ----loadtoy1,echo=FALSE-------------------------------------------------
+## ----loadtoy1,echo=FALSE------------------------------------------------------
 zz2 <- load_doc("toy1_runs.RData")
 
-## ----fakerun2,eval=FALSE-------------------------------------------------
+## ----fakerun2,eval=FALSE------------------------------------------------------
 #  d1 <- do_admb("toy1",
 #                data=tmpdat,
 #                params=list(beta=rep(0,ncol(X)),sigma_herd=0.1),
@@ -220,7 +220,7 @@ zz2 <- load_doc("toy1_runs.RData")
 #                mcmc=TRUE,
 #                mcmc.opts=mcmc.control(mcmc=20,mcmcpars=c("beta","sigma_herd")))
 
-## ----testprofinput,echo=FALSE,eval=FALSE---------------------------------
+## ----testprofinput,echo=FALSE,eval=FALSE--------------------------------------
 #  do_admb("toy1", data=tmpdat,
 #          params=list(beta=rep(0,ncol(X)),sigma_herd=0.1),
 #          bounds=list(sigma_herd=c(0.0001,20)),
@@ -230,20 +230,20 @@ zz2 <- load_doc("toy1_runs.RData")
 #  run_admb("toy1_gen",profile=TRUE)
 #  read_admb("toy1_gen",profile=TRUE)
 
-## ----coefsumlmer---------------------------------------------------------
+## ----coefsumlmer--------------------------------------------------------------
 ## FIXME
 ## coef(summary(gm1))
 
-## ----coefsumadmb---------------------------------------------------------
+## ----coefsumadmb--------------------------------------------------------------
 coef(summary(d1))[1:5,]
 
-## ----ranefs--------------------------------------------------------------
+## ----ranefs-------------------------------------------------------------------
 ## FIXME
 ## plot(ranef(gm1)$herd[,1],coef(d1)[6:20]*coef(d1)["sigma_herd"],
 ##     xlab="glmer estimate",ylab="ADMB estimate")
 ## abline(a=0,b=1)
 
-## ----mcmcconf------------------------------------------------------------
+## ----mcmcconf-----------------------------------------------------------------
 detach("package:lme4") ## HPDinterval definition gets in the way
 HPDinterval(as.mcmc(d1$mcmc[,6:20]))
 
